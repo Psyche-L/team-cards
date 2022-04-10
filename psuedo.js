cc7462de4ddf51f44765479f72ed5b08ec7d37d6
Pyche Laveau <homedome@Seveonnos-MacBook-Air.local>
2022-04-10 13:35:21 -0700

    html

---
+++ psuedo.js
@@ -0,0 +1,22 @@
+// promt to take in member information
+
+// generate HTML file populated with with the usesrs input
+
+// add event listeners when email is clicked in HTML
+
+// --defult email programs opens and populates TO feild of the email with address--
+
+// add event liseners to github user names of empoyees
+
+// Empoyees github opens in a new tab in the browser
+
+// promt the user to enter team managers name, employee ID, email, and office number
+
+// when the team managers info is given promt user to add an engineer or an intern or to finish building the team
+
+// take user to in a menue to add an engineer or an intern
+
+        // ingineer info : name, ID, email, gitHub user name
+        // interns info: name, ID, email, and school
+
+// generate html with the info        
